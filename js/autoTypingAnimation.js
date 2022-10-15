0ad12797c8073ac7195f47249ac79d8324960f48
REET KUAR BIND <104083295+Reetkumarbind@users.noreply.github.com>
2022-10-15 23:24:42 +0530

    Add files via upload

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span><h1> Hi ! </h1> I am Reet Kumar Bind </span> working as a software engineer at Technics Integration Technology. Overall 7+ Years experiance in web development. I have worked on many projects few of them are listed below. In college project i have build project on Contact Management System and during postgraduation I have built Project on Java Platform Cloud Data Protection for the masses. Kindly download below mentioned resume document for your ready refreance Technical Skill Set    •HTML       •	CSS        •	Java Script       •	Photoshop "
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
